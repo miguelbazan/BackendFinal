7fae9f94d769f5f1877d5b120b92203c20169725
Miguel Bazán <miguelb1995@icloud.com>
2019-05-06 17:17:12 -0500

    Ya jala=???

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -2,6 +2,13 @@ const mongoose = require('mongoose')
 
 const connectionURL = 'mongodb+srv://admin:xepwaR-dukhog-3nahbo@cluster0-cvzeq.mongodb.net/class?retryWrites=true'
 
+if (process.env.NODE_ENV === "production") {
+  var connectionURL = process.env.CONNECTION_URL;
+} else {
+  const { mongo } = require("../config.js");
+  var connectionURL = mongo;
+}
+
 mongoose.connect( connectionURL, {
   useNewUrlParser: true,
   useCreateIndex: true
