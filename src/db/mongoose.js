e3a75e8c756cb769ad7f91eadee1a381b37b14ec
Miguel Bazán <miguelb1995@icloud.com>
2019-05-06 17:24:28 -0500

    ya local

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,7 +1,5 @@
 const mongoose = require('mongoose')
 
-const connectionURL = 'mongodb+srv://admin:xepwaR-dukhog-3nahbo@cluster0-cvzeq.mongodb.net/class?retryWrites=true'
-
 if (process.env.NODE_ENV === "production") {
   var connectionURL = process.env.CONNECTION_URL;
 } else {
